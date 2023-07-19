473621bed18305652c2cb6104db6804557928506
Gqbo <gabriel.gonzaf@gmail.com>
2023-07-18 21:41:00 -0600

    Handle POST request in the sign-up route and flashes alert messages.

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,34 @@
 #A Blueprint is essentially a way to define a set of routes, views, and other application-related components in a modular manner. 
 # It helps to split a Flask application into reusable components, making the codebase more maintainable and extensible.
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", text="Testing")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+    
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Passwords must be greater than 6 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
+    return render_template("sign-up.html")
