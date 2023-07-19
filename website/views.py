586e68ef2308250719cb7b7befad5ad30b17e508
Gqbo <gabriel.gonzaf@gmail.com>
2023-07-18 20:21:39 -0600

    Renders a template instead of using the plain html.

--- website/views.py
+++ website/views.py
@@ -2,13 +2,13 @@
 
 #A Blueprint is essentially a way to define a set of routes, views, and other application-related components in a modular manner. 
 # It helps to split a Flask application into reusable components, making the codebase more maintainable and extensible.
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/') # Defines a runs the 'home page' when we call the root URL
 def home():
-    return "<h1>Test!<h1>"
+    return render_template("home.html")
 
 
 
